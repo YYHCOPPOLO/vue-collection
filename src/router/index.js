c624e37b0177b7e0af3bcb6fd781eb99e616246e
YYHCOPPOLO <1305403032@qq.com>
2023-05-15 20:59:53 +0800

    init: template constructure complete

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,77 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import App from "../App.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: App,
+    name: "App",
+    redirect: "/home",
+    children: [
+      {
+        path: "home",
+        component: () => import("../views/Home.vue"),
+        name: "Home",
+        children: [
+          {
+            path: "service",
+            component: () => import("../views/service/Service.vue"),
+            name: "Service",
+          },
+          {
+            path: "product",
+            component: () => import("../views/product/Product.vue"),
+            name: "Product",
+            children: [
+              {
+                path: "list",
+                component: () => import("../views/product/ProductList.vue"),
+                name: "ProductList",
+              },
+              {
+                path: "add/0",
+                component: () => import("../views/product/ProductEdit.vue"),
+                name: "ProductAdd",
+              },
+              {
+                path: "edit/:id",
+                component: () => import("../views/product/ProductEdit.vue"),
+                name: "ProductEdit",
+              },
+            ],
+          },
+          {
+            path: "custom-form",
+            component: () => import("../views/custom-form/index.vue"),
+            children: [
+              {
+                path: "form-to-json",
+                component: () => import("../views/custom-form/FormToJson.vue"),
+                name: "FormToJson",
+              },
+              {
+                path: "wysiwyg",
+                component: () => import("../views/custom-form/CustomForm.vue"),
+                name: "Wysiwyg",
+              },
+              {
+                path: "form-model",
+                component: () => import("../views/custom-form/FormModel.vue"),
+                name: "FormModel",
+              },
+            ],
+          },
+          {
+            path: "todo",
+            component: () => import("../views/todo/TodoList.vue"),
+            name: "TodoList",
+          },
+        ],
+      },
+    ],
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <Menu :isMenuCollapse="isMenuCollapse" />
+    <div class="router-view">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Menu from "../components/menu/Menu.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Menu,
+  },
+  data() {
+    return {
+      isMenuCollapse: false,
+    };
   },
 };
+// http://116.62.132.185/cms-api
+// http://codercba.prod:8000
+// http://111.230.245.205:8880
+// http://123.207.32.32:5000/
 </script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+  width: 100%;
+  display: flex;
+}
+
+.router-view {
+  flex: 1;
+}
+</style>
